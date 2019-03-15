d446dbfcfe1ae42eada4ef54a3f699fcdb6065f2
PX L <865635310@qq.com>
2019-03-15 23:10:26 +0800

    initial

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -58,10 +58,17 @@ int readelf(u_char *binary, int size)
                 return 0;
         }
 
-        // get section table addr, section header number and section header size.
-
+        // get section table addr, section header number and section header size
+	ptr_sh_table = binary + ehdr->e_shoff;
+	sh_entry_count = ehdr->e_shnum;
+	sh_entry_size = ehdr->e_shentsize;
+		
         // for each section header, output section number and section addr.
-
+	for (Nr = 0; Nr < sh_entry_count; Nr++) {
+		shdr = (Elf32_Shdr *)ptr_sh_table;
+		printf("%d:0x%x\n", Nr, shdr->sh_addr);
+		ptr_sh_table += sh_entry_size;
+	}
 
         return 0;
 }
